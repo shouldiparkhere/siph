4245aa838ce3b8a6d0c1e1e63a2a27b059633dd1
Chris Barnes <cjbarnes312@gmail.com>
2017-05-13 13:33:21 -0400

    need search bar

--- assets/siph.js
+++ assets/siph.js
@@ -1,4 +1,17 @@
-//Should I Park Here
+
+
+/*
+Syncing with the Master Branch
+0) COMMIT ALL OF THE CHANGES ON YOUR LOCAL BRANCH
+1) git checkout master
+2) git pull origin master
+3) git checkout MY_BRANCH
+4) git merge master
+Put Your Changes Into Master Branch
+0) COMMIT ALL OF THE CHANGES ON YOUR LOCAL BRANCH
+1) git checkout master
+2) git merge bryan
+*/
 
 // Initialize Firebase
  var config = {
@@ -15,8 +28,7 @@
 //define var for 1st auth key for first API
 
 var authKey1 = "privatekeyforspotcrimepublicusers-commercialuse-877.410.1607";
-var baseUrl = "https://api.spotcrime.com/crimes.json?lat=40.3718&lon=-73.9959179&radius=0.08&callback=jQuery21306930704791620661_1494546905160&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607&_=1494546905164"
-
+var baseUrl = "https://api.spotcrime.com/crimes.json?lat=39.9525838&lon=-75.165222&radius=0.08&callback=jQuery21306930704791620661_1494546905160&key=privatekeyforspotcrimepublicusers-commercialuse-877.410.1607&_=1494546905164"
 
 
 function runQuery() {
@@ -36,7 +48,79 @@ function runQuery() {
 runQuery();
 
 
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
 
+// Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      }
+
+  
 
 
 
@@ -45,7 +129,7 @@ runQuery();
 
 //define var for 2nd auth key
 
-var authKey2 = "";
+
 
 //global vars 
 
@@ -55,6 +139,3 @@ var authKey2 = "";
 //want our rating to be cleared out on load 
 
 //locate me button onclick loads up to google map api location finder
-
-
-
