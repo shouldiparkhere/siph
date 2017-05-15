b3833d6c69faef2fdba50e60dec86255a44a0352
Chris Barnes <cjbarnes312@gmail.com>
2017-05-15 15:23:28 -0400

    working geolocation

--- assets/siph.js
+++ assets/siph.js
@@ -25,6 +25,34 @@ Put Your Changes Into Master Branch
 
  firebase.initializeApp(config);
 
+//Should I Park Here
+
+/*
+Syncing with the Master Branch
+0) COMMIT ALL OF THE CHANGES ON YOUR LOCAL BRANCH
+1) git checkout master
+2) git pull origin master
+3) git checkout MY_BRANCH
+4) git merge master
+Put Your Changes Into Master Branch
+0) COMMIT ALL OF THE CHANGES ON YOUR LOCAL BRANCH
+1) git checkout master
+2) git merge bryan
+3) git thanks bryan for keeping your personal notes in
+*/
+
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDSqVrrCzIDY3FydRzWwBVrTwFAXHo0imU",
+   authDomain: "siph-1494544739001.firebaseapp.com",
+   databaseURL: "https://siph-1494544739001.firebaseio.com",
+   projectId: "siph-1494544739001",
+   storageBucket: "siph-1494544739001.appspot.com",
+   messagingSenderId: "524514555118"
+ };
+
+ firebase.initializeApp(config);
+
 //define var for 1st auth key for first API
 
 var authKey1 = "privatekeyforspotcrimepublicusers-commercialuse-877.410.1607";
@@ -47,20 +75,113 @@ function runQuery() {
 
 runQuery();
 
+// going to have to create some type of if then statement
+// that will filter wether the user is trying to search or 
+// if its getting their current location
 
 function initMap() {
+
         var uluru = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 13,
           center: uluru
         });
         var marker = new google.maps.Marker({
           position: uluru,
           map: map
         });
+};
+
+$(".locate-btn").click(function() {
+  var startPos;
+  var nudge = document.getElementById("nudge");
+
+  var showNudgeBanner = function() {
+    nudge.style.display = "block";
+  };
+
+  var hideNudgeBanner = function() {
+    nudge.style.display = "none";
+  };
+
+  var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+  var geoSuccess = function(position) {
+    hideNudgeBanner();
+    // We have the location, don't display banner
+    clearTimeout(nudgeTimeoutId);
+
+    // Do magic with location
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  var geoError = function(error) {
+    switch(error.code) {
+      case error.TIMEOUT:
+        // The user didn't accept the callout
+        showNudgeBanner();
+        break;
+    }
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+});
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
       }
 
-// Create the search box and link it to the UI element.
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+ function initAutocomplete() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
+        });
+
+        // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -120,7 +241,8 @@ function initMap() {
         });
       }
 
-  
+
+
 
 
 
@@ -132,6 +254,12 @@ function initMap() {
 
 
 //global vars 
+
+
+/* UPDATE - maybe create a lightbox that has the user enter, then
+opens up to the initial page with just the map and the search field
+and the button to track location. then have another lightbox display
+the score and the recent crimes in the area */
 
 
 //load up function 
